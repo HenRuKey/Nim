f73cecaaa9d50e8373550a3c33629ad3524b495a
Bryan <broberts@student.neumont.edu>
2018-10-06 20:12:01 -0600

    added overloaded constructor to GameController
    GUI pages from the design document have been added (still a work in progress) and user controls are being created. Data binding will be implemented on 8/7/18.

--- Nim/NimLibrary/GameController.cs
+++ Nim/NimLibrary/GameController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Nim.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,33 +10,64 @@ namespace NimLibrary
     public class GameController
     {
         Player player1;
-        PLayer player2;
+        Player player2;
+        Pile pile1 = new Pile();
+        Pile pile2 = new Pile();
+        Pile pile3 = new Pile();
+        Pile pile4 = new Pile();
         List<Pile> Piles = new List<Pile>(); 
         public GameController(string mode, string difficulty, string name1, string name2)
         {
-            player1 = new Player();
-            player1.Name = name1;
-            player1.IsComputer = false;
-            player2 = new PLayer();
-            player2.Name = name2();
-            player2.IsComputer = false;
+            player1 = new Player(name1, false);
             if (mode == "PvC")
             {
-                player2.IsComputer = true;
+                player2 = new Player(name2, true);
+            } else
+            {
+                player2 = new Player(name2, false);
             }
 
             switch (difficulty) {
                 case "easy":
-                    Pile pile1 = new Pile();
-                    Piles pile2 = new Pile();
-                    pile1.size = pile2.size = 3;
+                    pile1.Size = pile2.Size = 3;
+                    Piles.Add(pile1);
+                    Piles.Add(pile2);
                     break;
                 case "medium":
+                    pile1.Size = 2;
+                    pile2.Size = 5;
+                    pile3.Size = 7;
+                    Piles.Add(pile1);
+                    Piles.Add(pile2);
+                    Piles.Add(pile3);
                     break;
                 case "hard":
+                    pile1.Size = 2;
+                    pile1.Size = 3;
+                    pile1.Size = 8;
+                    pile1.Size = 9;
+                    Piles.Add(pile1);
+                    Piles.Add(pile2);
+                    Piles.Add(pile3);
+                    Piles.Add(pile4);
                     break;
             }
 
+            foreach (Pile pile in Piles)
+            {
+                for (int i = 0; i < pile.Size; i++)
+                {
+                    pile.Add(new Piece()
+                    {
+                        //ImagePath = "Add A Path Here",
+                        IsSelected = false
+                    });
+                }
+            }
+        }
+
+        public void TakeTurn()
+        {
 
         }
     }
